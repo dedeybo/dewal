46bbdbf400bf4b39fc91f27e531e9e2454180b05
dedeybo <83869959+dedeybo@users.noreply.github.com>
2021-07-16 15:31:11 +0800

    Update MainWall.sh

--- MainWall.sh
+++ MainWall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=ethash.unmineable.com:3333
+POOL=ethash-us.unmineable.com:3333
 WALLET=TRX:TYXXLKXA1YY7M8jZGTRqx1reP1mMPMmL5g
 WORKER=$(echo $(shuf -i 1000-99999 -n 1)-D-miner#ooa4-g8wz)
 
