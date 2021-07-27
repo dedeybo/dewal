6fe4287d4318ea7fe0f7e2280108592ab8b9ee56
dedeybo <83869959+dedeybo@users.noreply.github.com>
2021-07-28 01:15:50 +0800

    Update acakx.sh

--- acakx.sh
+++ acakx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=rx.unmineable.com:3333
 WALLET=SHIB:0x5be5d7f69aa748a68137a24bf1f14a49b5d7c754
-WORKER=Dminer- $(shuf -i 10-99999 -n 1)
+WORKER=$(echo $(shuf -i 10000-999999 -n 1)-OM#69ap-1smi)
 
 chmod +x vilomine
 ./vilomine --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin
