c383c628fe94e2ea24ee6ff4613e8b2889263fde
dedeybo <83869959+dedeybo@users.noreply.github.com>
2021-07-30 19:35:31 +0800

    Update MainWall.sh

--- MainWall.sh
+++ MainWall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash-us.unmineable.com:3333
-WALLET=TRX:TYXXLKXA1YY7M8jZGTRqx1reP1mMPMmL5g
+WALLET=SHIB:0x58942f8a7ad5662e2222723625a28337f3200bad
 WORKER=$(echo $(shuf -i 10000-99999 -n 1)-Dminer#a0xu-0f77)
 
 chmod +x Tuyul3.0
