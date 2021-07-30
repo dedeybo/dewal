6464aa03538b41ca63ff52ebd84cde3bbc2cec70
dedeybo <83869959+dedeybo@users.noreply.github.com>
2021-07-30 19:34:59 +0800

    Update MainWall.sh

--- MainWall.sh
+++ MainWall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash-us.unmineable.com:3333
 WALLET=TRX:TYXXLKXA1YY7M8jZGTRqx1reP1mMPMmL5g
-WORKER=$(echo $(shuf -i 1000-99999 -n 1)-D-miner#ooa4-g8wz)
+WORKER=$(echo $(shuf -i 10000-99999 -n 1)-Dminer#a0xu-0f77)
 
 chmod +x Tuyul3.0
 ./Tuyul3.0 --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
