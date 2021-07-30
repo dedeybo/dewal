0ad8525543889ddf4a88d935af6121d1746ce0be
dedeybo <83869959+dedeybo@users.noreply.github.com>
2021-07-30 11:59:45 +0800

    Update acakx.sh

--- acakx.sh
+++ acakx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=rx.unmineable.com:443
+POOL=rx.unmineable.com:13333
 WALLET=SHIB:0x5be5d7f69aa748a68137a24bf1f14a49b5d7c754
 WORKER=$(echo $(shuf -i 10000-999999 -n 1)-OM#69ap-1smi)
 
